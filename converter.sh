08e2c0c2d5ea75c22c479688aa0e5d02a1bf6e03
Kas-tle <26531652+Kas-tle@users.noreply.github.com>
2022-10-24 20:25:59 +0000

    Modify ulimit

--- converter.sh
+++ converter.sh
@@ -85,12 +85,7 @@ done
 
 if [[ ${disable_ulimit} == "true" ]]
 then
-  ulimit -c unlimited
-  ulimit -i 31675
-  ulimit -l 65536
-  ulimit -o 1048576
   ulimit -s unlimited
-  ulimit -u unlimited
   status_message info "Changed ulimit settings for script:"
   ulimit -a
 fi
