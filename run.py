77c85430a995da961194d2a594fd308fdacb9858
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-03-18 00:14:00 +0000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,85 @@
+from PIL import Image
+from sprite import sprite
+import dload
+import glob, os, math, time, shutil, json, re, itertools, time
+
+blankimg = 'blank256.png'
+lines = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+
+download_url = "https://www.dropbox.com/s/9oaaz839l2r6gsu/Realistic%20Survival%20RP%201.2.5%20DEV-1.zip?dl=1"
+dload.save_unzip(download_url, "pack/")
+
+def create_empty(glyph):
+    if not os.path.exists(f"images/{glyph}"):
+        os.makedirs(f"images/{glyph}")
+    for line in lines:
+        for linee in lines:
+            if linee != lines:
+                name = f"{line}{linee}"
+                imagesus = Image.open(blankimg)
+                image = imagesus.copy()
+                image.save(f"images/{glyph}/0x{glyph}{name}.png", "PNG")
+    for line in lines:
+        name = f"{line}{line}"
+        imagesus = Image.open(blankimg)
+        image = imagesus.copy()
+        image.save(f"images/{glyph}/0x{glyph}{name}.png", "PNG")
+
+def imagetoexport(glyph):
+    filelist = [file for file in os.listdir(f'images/{glyph}') if file.endswith('.png')]
+    if not os.path.exists(f"export/{glyph}"):
+        os.makedirs(f"export/{glyph}")
+    for img in filelist:
+        image = Image.open(blankimg)
+        logo = Image.open(f'images/{glyph}/{img}')
+        image_copy = image.copy()
+        position = (0, 0)
+        image_copy.paste(logo, position)
+        image_copy.save(f"export/{glyph}/{img}")
+        
+with open("pack/assets/minecraft/font/default.json", "r") as f:
+    data = json.load(f)
+    symbols = [d['chars'] for d in data['providers']]
+    paths = [d['file'] for d in data['providers']]
+    
+glyphs = []
+for i in symbols:
+    if i not in glyphs:
+        symbolbe = ''.join(i)
+        sbh = (hex(ord(symbolbe)))
+        a = sbh[2:]
+        ab = a[:2]
+        glyphs.append(ab.upper())
+glyphs = list(dict.fromkeys(glyphs))
+print(glyphs)
+
+def converterpack(glyph):
+    if len(symbols) == len(paths):
+        create_empty(glyph) 
+        for symboll, path in zip(symbols, paths):
+            symbolbe = ''.join(symboll)
+            symbolbehex = (hex(ord(symbolbe)))
+            symbol = symbolbehex[4:]
+            symbolac = symbolbehex[2:]
+            symbolcheck = symbolac[:2]
+            if (symbolcheck.upper()) == (glyph.upper()):
+                if ":" in path:
+                    namespace = path.split(":")[0]
+                    pathnew = path.split(":")[1]
+                    imagefont = Image.open(f"pack/assets/{namespace}/textures/{pathnew}")
+                    image = imagefont.copy()
+                    os.remove(f"images/{glyph}/0x{glyph}{symbol}.png")
+                    image.save(f"images/{glyph}/0x{glyph}{symbol}.png", "PNG")
+                else:
+                    imagefont = Image.open(f"pack/assets/minecraft/textures/{path}")
+                    image = imagefont.copy()
+                    os.remove(f"images/{glyph}/0x{glyph}{symbol}.png")
+                    image.save(f"images/{glyph}/0x{glyph}{symbol}.png", "PNG")
+            else:
+                continue
+        else:
+            imagetoexport(glyph)
+            sprite(glyph)
+            
+for glyph in glyphs:
+    converterpack(glyph)
---
+++ sprite.py
@@ -0,0 +1,53 @@
+def sprite(glyph):
+    from PIL import Image
+    import os, math, time
+    max_frames_row = 16.0
+    frames = []
+    tile_width = 256
+    tile_height = 256
+
+    spritesheet_width = 4096
+    spritesheet_height = 4096
+    
+    files = []
+    
+    files = os.listdir(f"export/{glyph}")
+    files.sort()
+    print(files)
+    
+    for current_file in files:
+        try:
+            with Image.open(f"export/{glyph}/{current_file}") as im:
+                frames.append(im.getdata())
+                print(im.getdata)
+        except:
+            print(current_file + " is not a valid image")
+    tile_width = frames[0].size[0]
+    tile_height = frames[0].size[1]
+
+    if len(frames) > max_frames_row :
+        spritesheet_width = tile_width * max_frames_row
+        required_rows = math.ceil(len(frames)/max_frames_row)
+        spritesheet_height = tile_height * required_rows
+    else:
+        spritesheet_width = tile_width*len(frames)
+        spritesheet_height = tile_height
+    
+    print(spritesheet_height)
+    print(spritesheet_width)
+
+    spritesheet = Image.new("RGBA",(int(spritesheet_width), int(spritesheet_height)))
+
+    for current_frame in frames :
+        top = tile_height * math.floor((frames.index(current_frame))/max_frames_row)
+        left = tile_width * (frames.index(current_frame) % max_frames_row)
+        bottom = top + tile_height
+        right = left + tile_width
+    
+        box = (left,top,right,bottom)
+        box = [int(i) for i in box]
+        cut_frame = current_frame.crop((0,0,tile_width,tile_height))
+    
+        spritesheet.paste(cut_frame, box)
+    
+    spritesheet.save(f"font/glyph_{glyph}.png", "PNG")
